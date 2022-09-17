51900f5d411c7303d5dd9dd169488fae07319606
Anastasia Gainulina <Famousnastya@gmail.com>
2022-09-18 01:21:03 +0300

    array,length

---
+++ Ex_03/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+int []array = {11,92,33,46,45,16,27,84};
+
+int n = array.Length;
+
+int find = 46;
+
+int index = 0;
+
+while( index <n )
+{
+   if(array[index]==find)
+   {
+    System.Console.WriteLine(index);
+    break;
+   }
+    // index= index+1;
+    index++;
+}
